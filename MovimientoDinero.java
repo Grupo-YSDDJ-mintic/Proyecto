dc800942c0e4db540c65150f546f4466efff5d75
David Gonzalez <davidgonzalez5910@gmail.com>
2022-08-25 11:34:57 -0500

    nueva clase MovimientoDinero

---
+++ MovimientoDinero.java
@@ -0,0 +1,34 @@
+public class MovimientoDinero {
+    private double montoMovimiento;
+    private String motivoMovimiento, usuario;
+
+    MovimientoDinero(double montoMovimiento, String motivoMovimiento, String usuario) {
+        this.montoMovimiento = montoMovimiento;
+        this.motivoMovimiento = motivoMovimiento;
+        this.usuario = usuario;
+    }
+
+    public int getMontoMovimiento() {
+        return montoMovimiento;
+    }
+
+    public void setMontoMovimiento(int montoMovimiento) {
+        this.montoMovimiento = montoMovimiento;
+    }
+
+    public String getMotivoMovimiento() {
+        return motivoMovimiento;
+    }
+
+    public void setMotivoMovimiento(String motivoMovimiento) {
+        this.motivoMovimiento = motivoMovimiento;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+}
