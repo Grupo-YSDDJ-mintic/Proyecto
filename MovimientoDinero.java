4adf1cd810cb7aee1643d45cac028997a162da4f
difegus <111029634+difegus@users.noreply.github.com>
2022-08-28 00:59:42 -0500

    Movimiento dinero feature (#2)
    * nueva clase MovimientoDinero
    
    * Cambie el return type del setter montoMovimiento
    
    * Modificación del setMontoMovimiento
    
    * se elimina el método setUsuario
    
    Co-authored-by: David Gonzalez <davidgonzalez5910@gmail.com>

---
+++ MovimientoDinero.java
@@ -0,0 +1,31 @@
+public class MovimientoDinero {
+    private double montoMovimiento;
+    private String motivoMovimiento, usuario;
+
+    MovimientoDinero(double montoMovimiento, String motivoMovimiento, String usuario) {
+        this.montoMovimiento = montoMovimiento;
+        this.motivoMovimiento = motivoMovimiento;
+        this.usuario = usuario;
+    }
+
+    public double getMontoMovimiento() {
+        return montoMovimiento;
+    }
+
+    public void setMontoMovimiento(double montoMovimiento) {
+        this.montoMovimiento = montoMovimiento;
+    }
+
+    public String getMotivoMovimiento() {
+        return motivoMovimiento;
+    }
+
+    public void setMotivoMovimiento(String motivoMovimiento) {
+        this.motivoMovimiento = motivoMovimiento;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+ }
